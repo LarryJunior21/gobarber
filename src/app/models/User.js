c6b4130ae66cca849c9f0787355af47d3d527cd8
Larry Junior <larryjunior21@hotmail.com>
2020-02-02 21:56:52 -0300

    token jwt, user update, data valitation

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,6 +2,7 @@ import { Model, Sequelize } from 'sequelize';
 import bcrypt from 'bcryptjs';
 
 class User extends Model {
+
   static init(sequelize) {
     super.init(
       {
@@ -12,6 +13,7 @@ class User extends Model {
         provider: Sequelize.BOOLEAN,
       },
       {
+        //VARIAVEL PARA TRADUÇÃO DE CODIGO JAVASCRIPT PARA SQL
         sequelize,
       }
     );
@@ -23,6 +25,12 @@ class User extends Model {
     });
 
     return this;
+  }
+
+  checkPassword(password) {
+    //FAZ A ENCRIPTAÇÃO DA SENHA E VERIFICA SE AS DUAS SÃO IGUAIS
+    //POIS NO BANCO É GUARDADA APENAS A SENHA GERADA COM O HASH
+    return bcrypt.compare( password, this.password_hash );
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import UserController from './app/controllers/userController';
+import SessionController from './app/controllers/sessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/update', UserController.update);
 
 export default routes;
