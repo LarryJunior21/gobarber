027849fc5a76483a2c52d81c9aeef4598b9be569
Larry Junior <larryjunior21@hotmail.com>
2020-02-19 13:18:35 -0300

    styled signup and signin

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Exemplo: teste@teste.com')
+    .required('*Digite um email'),
+  password: Yup.string().required('*Digite uma senha'),
+});
 
 export default function SignIn() {
-  return <div />;
+  function handleSubmit(data) {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="DeskPoint" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar uma nova conta</Link>
+      </Form>
+    </>
+  );
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
-  return <div />;
+export default function SignIn() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('*Digite seu nome'),
+    email: Yup.string()
+      .email('E-mail inválido, exemplo: teste@teste.com')
+      .required('*Digite um email'),
+    password: Yup.string()
+      .min(6, '*A senha deve ter pelo menos 6 digitos/caracteres')
+      .required('*Digite uma senha'),
+  });
+
+  function handleSubmit(data) {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="DeskPoint" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Cadastrar</button>
+        <Link to="/">Já possui cadastro? Faça Login</Link>
+      </Form>
+    </>
+  );
 }
