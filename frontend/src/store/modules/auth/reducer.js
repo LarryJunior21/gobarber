03206746fbbadd87e26de10e3810674a92c233de
Larry Junior <larryjunior21@hotmail.com>
2020-02-26 20:32:08 -0300

    redux saga reactotron

---
+++ frontend/src/store/modules/auth/reducer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const INITIAL_STATE = {};
+
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ frontend/src/store/modules/auth/sagas.js
@@ -0,0 +1,5 @@
+import { all } from 'redux-saga/effects';
+
+// import { Container } from './styles';
+
+export default all([]);
