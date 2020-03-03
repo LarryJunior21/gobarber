7ac27e27369a3f74433022bd801e624db7a24273
Larry Junior <larryjunior21@hotmail.com>
2020-03-03 19:39:03 -0300

    update user

--- frontend/src/components/Notifications/index.js
+++ frontend/src/components/Notifications/index.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
-// import { Container } from './styles';
+import { MdNotifications } from 'react-icons/md';
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Notification,
+  Scroll,
+} from './styles';
+
+import api from '~/services/api';
 
 export default function Notifications() {
-  return <div />;
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  const hasUnread = useMemo(
+    () => !!notifications.find(notification => notification.read === false),
+    [notifications]
+  );
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          { addSuffix: true, locale: pt }
+        ),
+      }));
+
+      setNotifications(data);
+    }
+
+    loadNotifications();
+  }, []);
+
+  function _handleVisible() {
+    setVisible(!visible);
+  }
+
+  async function _handleRead(id) {
+    await api.put(`notifications/${id}`);
+
+    setNotifications(
+      notifications.map(notification =>
+        notification._id === id ? { ...notification, read: true } : notification
+      )
+    );
+  }
+
+  return (
+    <Container>
+      <Badge onClick={_handleVisible} hasUnread={hasUnread}>
+        <MdNotifications color="#10acdd" size={20} />
+      </Badge>
+
+      <NotificationList visible={visible}>
+        <Scroll>
+          {notifications.map(notification => (
+            <Notification unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              {!notification.read && (
+                <button
+                  type="button"
+                  onClick={() => _handleRead(notification._id)}
+                >
+                  Marcar como lida
+                </button>
+              )}
+            </Notification>
+          ))}
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
 }
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>aaaaaaaaaaaaaa</h1>;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function _handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={_handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua nova senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do DeskPoint</button>
+    </Container>
+  );
 }
