3f346a3e777211a01a9618446d0566b5ca7b7508
Larry Junior <larryjunior21@hotmail.com>
2020-03-18 19:42:36 -0300

    input done

--- mobile/mobarber/src/pages/SignIn/index.js
+++ mobile/mobarber/src/pages/SignIn/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Text} from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>PIROCA</Text>
+      <Text>SIGN SignIn</Text>
+
+      <Input style={{marginTop: 30}} icon="heart" placeholder="piroca" />
     </Background>
   );
 }
